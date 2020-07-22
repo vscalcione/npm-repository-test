3f03c81cacaac48847038eb30b86c1d663273a4d
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-22 22:39:35 +0200

    Add storybook documentation

--- fb-library-playground/projects/fb-library/src/stories/index.ts
+++ fb-library-playground/projects/fb-library/src/stories/index.ts
@@ -1,4 +1,5 @@
 import { storiesOf } from '@storybook/angular';
+import { action } from '@storybook/addon-actions';
 import { CardComponent } from '../lib/components/card/card.component';
 
 storiesOf('Card', module)
@@ -14,5 +15,25 @@ storiesOf('Card', module)
     props: {
       title: 'i\'m closed card',
       opened: false
+    }
+  }))
+  .add('with content projection', () => ({
+    moduleMetaData: {
+      declarations: [CardComponent]
+    },
+    template: `
+      <fb-card title="hi" [opened]="true">
+        <input type="text">
+        <input type="text">
+        <input type="text">
+        <input type="text">
+      </fb-card>
+    `
+  }))
+  .add('with actions', () => ({
+    component: CardComponent,
+    props: {
+      title: 'click me',
+      toggle: action('toggle')
     }
   }));
